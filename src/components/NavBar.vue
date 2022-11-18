493434394ec398321b8ab117ff141616b610587e
emendo42 <erickmendoza0960@gmail.com>
2022-11-18 13:36:19 -0600

    moved images from images to public/pictures

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,11 @@
 <template>
   <nav>
-    <img class="logo" :src="'/images/logo.png'" alt="Cowabunga Comics & Collectibles" srcset="/images/logo.png" />
+    <img
+      class="logo"
+      :src="'/public/pictures/logo.png'"
+      alt="Cowabunga Comics & Collectibles"
+      srcset="/public/pictures/logo.png"
+    />
     <div class="navWrapper">
       <RouterLink class="home" to="/">Home</RouterLink>
       <RouterLink class="login" to="/login">LoginPage</RouterLink>
@@ -17,9 +22,18 @@ nav {
     @apply mx-auto flex w-6/12 items-center bg-transparent;
   }
   .navWrapper {
-    @apply container mx-auto flex w-full items-center justify-evenly pt-1;
+    @apply container mx-auto flex w-full items-center justify-evenly bg-red-700 py-1;
     .home {
-      @apply bg-slate-500 px-6;
+      @apply bg-slate-500 px-1.5 hover:bg-white hover:text-black;
+    }
+    .login {
+      @apply bg-slate-500 px-6  hover:bg-white hover:text-black;
+    }
+    .aboutus {
+      @apply bg-slate-500 px-6  hover:bg-white hover:text-black;
+    }
+    .reviews {
+      @apply bg-slate-500 px-6 hover:bg-white hover:text-black;
     }
   }
 }
