5dc142b72fccf956d8ecd9b42895a1852e11544f
emendo42 <erickmendoza0960@gmail.com>
2022-11-29 19:55:21 -0600

    updated about us page

--- src/components/AboutUsPage.vue
+++ src/components/AboutUsPage.vue
@@ -1,21 +1,23 @@
 <template>
   <h1>&nbsp;</h1>
   <h2 class="address">We are located at 1500 s 16th st, McAllen, in the great state of Texas</h2>
-  <h3 class="hours">
-    🕙Monday &nbsp; &nbsp; &nbsp; &nbsp; 10am👉10pm
-    <br />
-    🕙Tuesday &nbsp; &nbsp; &nbsp; &nbsp; 10am👉10pm
-    <br />
-    🕙Wednesday &nbsp; 10am👉10pm
-    <br />
-    🕙Thrusday &nbsp; &nbsp; &nbsp; 10am👉10pm
-    <br />
-    🕙Friday &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 10am👉10pm
-    <br />
-    🕙Saturday &nbsp; &nbsp; &nbsp; 10am👉10pm
-    <br />
-    🕙Sunday &nbsp; &nbsp; &nbsp; &nbsp; 1pm👉 8pm
-  </h3>
+  <div class="hours_box">
+    <h3 class="hours">
+      🕙Monday &nbsp; &nbsp; &nbsp; &nbsp; 10am👉10pm
+      <br />
+      🕙Tuesday &nbsp; &nbsp; &nbsp; &nbsp; 10am👉10pm
+      <br />
+      🕙Wednesday &nbsp; 10am👉10pm
+      <br />
+      🕙Thrusday &nbsp; &nbsp; &nbsp; 10am👉10pm
+      <br />
+      🕙Friday &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 10am👉10pm
+      <br />
+      🕙Saturday &nbsp; &nbsp; &nbsp; 10am👉10pm
+      <br />
+      🕙Sunday &nbsp; &nbsp; &nbsp; &nbsp; 1pm👉 8pm
+    </h3>
+  </div>
   <h4 class="visit">Come Visit us, virtually all day to fuel your addiction in the following:</h4>
   <h5 class="addictionItems">
     💥Comics..............(duhh)
@@ -44,8 +46,11 @@
 .address {
   @apply flex  justify-center bg-red-200  pl-10;
 }
-.hours {
-  @apply flex  justify-center bg-red-200  pl-10;
+.hours_box {
+  @apply grid max-w-full justify-items-center;
+  .hours {
+    @apply grid w-8/12 grid-flow-row justify-items-center bg-red-400;
+  }
 }
 .addictionItems {
   @apply flex  justify-center bg-red-200;
