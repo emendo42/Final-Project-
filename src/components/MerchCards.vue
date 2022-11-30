f0ce07604c685693308964fec6670b303311cb67
emendo42 <erickmendoza0960@gmail.com>
2022-11-29 19:55:44 -0600

    changed colum grid on smaller screens

--- src/components/MerchCards.vue
+++ src/components/MerchCards.vue
@@ -10,6 +10,6 @@ import MerchCardsSingle from '@/components/MerchCardsSingle.vue'
 
 <style scoped lang="postcss">
 .merch_area {
-  @apply grid grid-cols-1 gap-9  bg-green-500 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6;
+  @apply grid grid-cols-2 gap-9  bg-green-500 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6;
 }
 </style>
