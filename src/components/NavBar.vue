a2bc6e1ed572f97487ded7c4a2b38b0c4e0413bd
emendo42 <erickmendoza0960@gmail.com>
2022-12-05 20:25:09 -0600

    added user email and log out to navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,7 +2,7 @@
 import { ref } from 'vue'
 import { useAuth } from '@/components/composables/useAuth'
 
-const { isAuthenticated, logout } = useAuth()
+const { isAuthenticated, logout, user } = useAuth()
 </script>
 
 <template>
@@ -13,13 +13,24 @@ const { isAuthenticated, logout } = useAuth()
       alt="Cowabunga Comics & Collectibles"
       srcset="/src/assets/pictures/logo.png"
     />
+    <div v-if="isAuthenticated" class="Username">
+      <p v-show="isAuthenticated" class="email">
+        Whats up 🍕
+        <strong>
+          <i>{{ user?.email }}</i>
+          <button class="logout" @click="logout">🥱 Logout Dude</button>
+        </strong>
+      </p>
+    </div>
+
     <div class="navWrapper">
       <RouterLink :to="{ name: 'Home' }" class="home">Home</RouterLink>
       <RouterLink class="aboutus" to="/aboutus">AboutUs</RouterLink>
-      <div v-if="isAuthenticated" class="auth">
+
+      <div v-if="isAuthenticated">
         <RouterLink :to="{ name: 'Reviews' }" href="#" class="reviews">Reviews</RouterLink>
-        <button class="logout" @click="logout">🥱 Logout Dude</button>
       </div>
+
       <div v-else>
         <RouterLink :to="{ name: 'LoginPage' }" href="#" class="login">LoginPage</RouterLink>
       </div>
@@ -32,6 +43,15 @@ nav {
   @apply flex h-4/5  flex-col bg-slate-400 text-slate-300;
   .logo {
     @apply mx-auto flex w-6/12 items-center bg-transparent;
+  }
+  .Username {
+    @apply flex flex-col justify-center bg-red-400 text-sm font-semibold text-black;
+  }
+  .email {
+    @apply pl-4;
+  }
+  .logout {
+    @apply ml-40 font-mono font-bold text-black  hover:text-white;
   }
   .navWrapper {
     @apply container mx-auto flex w-screen items-center justify-evenly bg-red-700 py-1;
@@ -46,12 +66,6 @@ nav {
     }
     .reviews {
       @apply bg-slate-400 px-1.5 font-mono font-bold text-black hover:bg-white hover:text-black;
-    }
-    .auth {
-      @apply -mx-20;
-    }
-    .logout {
-      @apply bg-red-700   font-mono font-bold text-black  hover:text-white;
     }
   }
 }
